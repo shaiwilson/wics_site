2820ecfe1787bb972c9290d217ab12cbd413e3a8
Shai Wilson <sjwilson2@usfca.edu>
2015-10-19 20:37:12 -0700

    Add EJS template

--- app.js
+++ app.js
@@ -3,20 +3,22 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
 
 var app = module.exports = express.createServer();
+var engine = require('ejs-locals');
+app.engine('ejs', engine);
+
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
 app.use(express.compiler({ src : __dirname + '/public', enable: ['less']}));
-  app.use(app.router);
+  //app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
@@ -27,6 +29,7 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.use(express.errorHandler());
 });
+
 
 // Compatible
 
@@ -40,8 +43,19 @@ express.compiler.compilers.less.compile = function(str, fn){
 }
 
 // Routes
+app.get('/', function(req, res){
+    res.render('index', { title: 'USF Women in Computing' });
+});
 
-app.get('/', routes.index);
+// This responds a GET request for the /contact page.
+app.get('/contact', function(req, res) {
+  res.render('contact');
+})
+
+// This responds a GET request for /about
+app.get('/about', function(req, res) {   
+   res.render('about');
+})
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index.html', { title: 'USF Women in Computing' })
+};
+
+exports.index = function(req, res){
+  res.render('contact.html', { title: 'USF Women in Computing' })
+};
+
+exports.index = function(req, res){
+  res.render('about.html', { title: 'USF Women in Computing' })
 };
